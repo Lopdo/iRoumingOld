aade80c207b5f6d7054cc70bbc2c43a57bbff3d2
Lopdo <lopenka@gmail.com>
2015-11-20 18:34:48 +0100

    - checking validity of string url when loading image

--- iRouming/utils/UIImageView+Alamofire.swift
+++ iRouming/utils/UIImageView+Alamofire.swift
@@ -10,15 +10,25 @@ import UIKit
 import Alamofire
 
 extension UIImageView {
-	public func imageFromUrl(urlString: String) -> Request {
-		return Alamofire.request(.GET, urlString).response { (request, response, data, error) in
-			self.image = UIImage(data: data!, scale:1)
+	public func imageFromUrl(urlString: String) -> Request? {
+		// check if the urlString is valid URL
+		if NSURL(string: urlString) != nil {
+			return Alamofire.request(.GET, urlString).response { (request, response, data, error) in
+				self.image = UIImage(data: data!, scale:1)
+			}
+		} else {
+			return nil
 		}
 	}
 	
-	public func imageFromUrl(urlString: String, result: (image: UIImage?) -> Void) -> Request {
-		return Alamofire.request(.GET, urlString).response { (request, response, data, error) in
-			result(image: UIImage(data: data!, scale:1))
+	public func imageFromUrl(urlString: String, result: (image: UIImage?) -> Void) -> Request? {
+		// check if the urlString is valid URL
+		if NSURL(string: urlString) != nil {
+			return Alamofire.request(.GET, urlString).response { (request, response, data, error) in
+				result(image: UIImage(data: data!, scale:1))
+			}
+		} else {
+			return nil
 		}
 	}
 }
