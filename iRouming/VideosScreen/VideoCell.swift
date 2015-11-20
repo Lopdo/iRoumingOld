43bb1edf5924803737994f8fe1cffa228cdbef09
Lopdo <lopenka@gmail.com>
2015-11-20 22:11:48 +0100

    - finished

--- iRouming/VideosScreen/VideoCell.swift
+++ iRouming/VideosScreen/VideoCell.swift
@@ -8,11 +8,39 @@
 
 import UIKit
 
-class VideoCell: UICollectionViewCell {
+class VideoCell: UICollectionViewCell, UIWebViewDelegate {
 
+	@IBOutlet var webView: UIWebView!
+	@IBOutlet var activityIndicator: UIActivityIndicatorView!
+	
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+		
+		webView.scrollView.scrollEnabled = false
     }
 
+	func setVideoObject(video: VideoObject) {
+		webView.hidden = true
+	
+		let width = self.frame.size.width
+		let height = self.frame.size.height
+		
+		do {
+			let youTubeVideoHTML = try String(contentsOfFile: NSBundle.mainBundle().pathForResource("youtubeEmbed", ofType: "html")!, encoding: NSUTF8StringEncoding)
+			let autoPlayString = ""
+			//let autoPlayString = autoPlay ? "events: {'onReady' : onPlayerReady }," : "" //customise it weather u want to autoplay or not
+			let controls = 1 //i want to show controles for play,pause,fullscreen ... etc
+			let html = String(format: youTubeVideoHTML, arguments: [video.youtubeId, width, height, autoPlayString, controls]) //setting the youtube video width and height to fill entire the web view
+			webView.mediaPlaybackRequiresUserAction = false
+			webView.delegate = self
+			webView.loadHTMLString(html, baseURL:NSBundle.mainBundle().resourceURL) //load it to webview
+		} catch {
+			
+		}
+	}
+	
+	func webViewDidFinishLoad(webView: UIWebView) {
+		webView.hidden = false
+		activityIndicator.stopAnimating()
+	}
 }
--- iRouming/VideosScreen/VideosViewController.swift
+++ iRouming/VideosScreen/VideosViewController.swift
@@ -8,28 +8,129 @@
 
 import UIKit
 
-class VideosViewController: CommentableViewController {
+class VideosViewController: CommentableViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
+	@IBOutlet var collectionView: UICollectionView!
+	@IBOutlet var labelTitle: UILabel!
+	@IBOutlet var buttonRefresh: UIButton!
+	@IBOutlet var activityIndicator: UIActivityIndicatorView!
+	@IBOutlet var imageViewRating: UIImageView!
+	
+	var prevIndexPath: NSIndexPath? = nil
+	
+	var videosManager = VideosManager()
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+		NSNotificationCenter.defaultCenter().addObserver(self, selector: "videosLoaded", name: kVideosLoadedNotification, object: nil)
+		
+		buttonRefresh.hidden = true
+		activityIndicator.startAnimating()
+		
+		videosManager.loadVideos()
+		
+		imageViewRating.image = nil
+		buttonComments.hidden = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+	deinit {
+		NSNotificationCenter.defaultCenter().removeObserver(self)
+	}
+	
+	override func viewWillAppear(animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		GANTracker.sharedInstance.trackView("videa")
+	}
 
-    /*
-    // MARK: - Navigation
+	// MARK: - UICollectionView
+	
+	func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return videosManager.videos.count
+	}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCellWithReuseIdentifier("VideoCell", forIndexPath: indexPath) as! VideoCell
+		
+		cell.setVideoObject(videosManager.videos[indexPath.row])
+		
+		return cell
+	}
+	
+	func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+		return collectionView.frame.size
+	}
+	
+	// MARK: - Data
+	
+	func videosLoaded()
+	{
+		collectionView.reloadData()
+		buttonRefresh.hidden = false
+		activityIndicator.stopAnimating()
+		
+		if videosManager.videos.count > 0 {
+			let videoObj = videosManager.videos[0]
+			
+			labelTitle.text = videoObj.name
+			imageViewRating.image = UIImage(named: "icon_rating_" + String(videoObj.rating))
+			buttonComments.hidden = false
+			
+			self.currentObject = videoObj
+		}
+	}
+	
+	@IBAction func onRefresh(sender: UIButton) {
+		buttonRefresh.hidden = true
+		activityIndicator.startAnimating()
+		videosManager.loadVideos()
+	}
+	
+	// MARK: - Rotation
+	
+	override func willRotateToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
+		super.willRotateToInterfaceOrientation(toInterfaceOrientation, duration: duration)
+		
+		self.prevIndexPath = collectionView.indexPathsForVisibleItems()[0]
+		collectionView.reloadData()
+		collectionView.hidden = true
+		collectionView.collectionViewLayout.invalidateLayout()
+	}
+	
+	override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
+		super.didRotateFromInterfaceOrientation(fromInterfaceOrientation)
+		
+		collectionView.scrollToItemAtIndexPath(self.prevIndexPath!, atScrollPosition: .CenteredHorizontally, animated: false)
+		collectionView.hidden = false
+	}
 
+	func scrollViewDidScroll(scrollView: UIScrollView) {
+		let index = Int((scrollView.contentOffset.x + scrollView.frame.size.width / 2) / scrollView.frame.size.width)
+		
+		if videosManager.videos.count > 0 {
+			let videoObj = videosManager.videos[index]
+			
+			labelTitle.text = videoObj.name
+			imageViewRating.image = UIImage(named: "icon_rating_" + String(videoObj.rating))
+			buttonComments.hidden = false
+			
+			self.currentObject = videoObj
+		}
+	}
+	
+	@IBAction func onComments(sender: UIButton) {
+		self.toggleComments()
+	}
+	
+	override func getCurrentObject() -> FunnyObject? {
+		let visibleRect = CGRectMake(collectionView.contentOffset.x, collectionView.contentOffset.y, collectionView.bounds.width, collectionView.bounds.height)
+		let visiblePoint = CGPointMake(CGRectGetMidX(visibleRect), CGRectGetMidY(visibleRect))
+		if let visibleIndexPath = collectionView.indexPathForItemAtPoint(visiblePoint) {
+			return videosManager.videos[visibleIndexPath.row]
+		} else {
+			return nil
+		}
+	}
+	
 }
